328134823426edf16b2bd927bc64e37955eda965
Dwayne Campbell <dwaynecampbell13@gmail.com>
2011-10-09 21:21:15 -0400

    User Sign Up Complete(skipped rspec :( )

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,18 @@ class UsersController < ApplicationController
 
   def new
 	@title ="Sign Up"
+	@user = User.new
   end
 
+  def create
+	@user = User.new(params[:user])
+	if @user.save
+	flash[:success]= "Welcome to the Sample App!"
+	redirect_to @user	
+	else
+	@title = "Sign Up"
+	@user.password = @user.password_confirmation = nil
+	render 'new'
+  	end
+  end
 end
