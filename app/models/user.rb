f72e6472c4894ed2ff012e356342a66b039f9193
Dwayne Campbell <dwaynecampbell13@gmail.com>
2011-11-12 12:49:50 -0500

    Added microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ require 'digest'
 class User < ActiveRecord::Base
 	attr_accessor :password
 	attr_accessible :name, :email, :password, :password_confirmation
-
+	has_many :microposts, :dependent => :destroy
 
 	email_regex =/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -18,6 +18,11 @@ class User < ActiveRecord::Base
 			     :length => { :within => 6..40}
 
 	before_save :encrypt_password
+
+	def feed
+	  Micropost.where("user_id = ?", id)
+	end
+
 
 	def has_password?(submitted_password)
 		encrypted_password == encrypt(submitted_password)
